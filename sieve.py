7f63f60e21a7c61ce9c5a1c5df2e0bef1c090a82
ghst659 <gst659@gmail.com>
2021-12-29 03:08:28 -0800

    Square optimisation.

--- sieve.py
+++ sieve.py
@@ -5,19 +5,14 @@ import logging
 
 def sieve(n: int) -> list[int]:
     """"Returns the list of prime numbers up to and including N."""
-    return sieve_array(n)
-
-def sieve_array(n: int) -> list[int]:
-    """"Array implementation."""
     if n < 2:
         return []
-    is_prime = [True] * n
-    is_prime[0] = False
-    for i in range(n):
-        if not is_prime[i]:
+    size = n + 1
+    is_prime = [True] * size  # One slot for each int up to n.
+    is_prime[0:2] = (False, False)  # 0 and 1 are not primes.
+    for p in range(size):
+        if not is_prime[p]:
             continue
-        p = i + 1
-        for v in range(2 * p, n + 1, p):
-            is_prime[v - 1] = False
-    result = [i + 1 for i in range(n) if is_prime[i]]
-    return result
+        for v in range(p * p, size, p):
+            is_prime[v] = False
+    return [p for p in range(size) if is_prime[p]]
